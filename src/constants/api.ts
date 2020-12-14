623fb94b9af9d134166080fa4ab13f7c28760f2d
devleonardorabelo <devleonardorabelo@gmail.com>
2020-12-14 20:36:37 -0300

    feat: added API service

---
+++ src/constants/api.ts
@@ -0,0 +1,1 @@
+export const API_URL = 'http://192.168.1.95:3030';
---
+++ src/services/api.ts
@@ -0,0 +1,17 @@
+import axios, { Method } from 'axios';
+import { API_URL } from '../constants/api';
+
+const request = (route: string, method?: Method) => {
+  return axios({
+    url: `${API_URL}/${route}`,
+    method: method || 'get',
+  })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
+
+export default request;
